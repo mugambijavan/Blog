ac8d9ff421c309351355f612797421f8cb17c28a
Mugambi Javan <mugambijavan@gmail.com>
2023-09-21 13:02:41 +0300

    hello

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ab1ca543e8bec786"; }
+/******/ 		__webpack_require__.h = function() { return "ef9397eefe64d62a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
