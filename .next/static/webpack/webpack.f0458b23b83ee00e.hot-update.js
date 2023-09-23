ebe01d33383213cea0a726be97026c9a7ac48464
Mugambi Javan <mugambijavan@gmail.com>
2023-09-23 20:53:03 +0300

    deploy

--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "6e3d5310694a0892"; }
+/******/ 	__webpack_require__.h = function() { return "d45e800880159116"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
